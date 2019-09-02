997c2b5d17338126ad3329ab9798bce991493f5a
Raja Raju <25823208+GRajaraju@users.noreply.github.com>
2019-09-02 14:44:22 +0530

    Update generate_faces.py

--- generate_faces.py
+++ generate_faces.py
@@ -18,7 +18,7 @@ batchSize = 64
 
 transform = transforms.Compose([transforms.Resize(image_size),
                                 transforms.ToTensor(),
-                                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+                                ])
 
 data_dir='./processed-celeba-small/'
 
